f62dbbab1c2b65730f1d89537f33b7d63336a3b0
Glauber Gonçalves <glauber_g3@yahoo.com.br>
2017-01-02 17:35:01 -0200

    Criação de 4 funções para busca e analise

--- scanner.py
+++ scanner.py
@@ -1,9 +1,11 @@
 from tkinter import *
-from socket import *
+import socket
 import os, platform
 
-
+#
+# Função ping retorna true se IP estiver ativo na rede e False se não estiver
 #-------------------------------------------------------------------------------#
+
 def ping(host):    
     # ping de acordo com SO
     ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
@@ -13,9 +15,88 @@ def ping(host):
     if (relatorio[-5]+relatorio[-4]) != 'da':
         return True
     else: return False
+#
+# retorna uma lista com cada valor do ip
 #-------------------------------------------------------------------------------#
-    
-for i in range(256):
-    host = "179.198.138." + str(i)
-    if(ping(host)):
-        print("host "+host+": Ativo na rede")
+def iphost(host):
+    ip = socket.gethostbyname(host)
+    ip = ip.split('.')
+    return ip
+#
+# 
+#-------------------------------------------------------------------------------#
+def verifAparelho(ip,dmin,dmax,cmin,cmax):
+    if(ip[0] == '10'):
+        for i in range(dmin,dmax+1):
+            ip[3] = str(i)
+            mont = ip[0]+"."+ip[1]+"."+ip[2]+"."+ip[3]
+            print(mont)
+            if(ping(mont)):
+                visor.insert('insert',"AP ativo: " + mont + "\n")
+                janela.update()                
+    else:
+        for i in range(cmin,cmax+1):        
+            ip[3] = str(i)
+            mont = ip[0]+"."+ip[1]+"."+ip[2]+"."+ip[3]
+            print(mont)
+            if(ping(mont)):
+                visor.insert('insert',"AP ativo: " + mont + "\n")
+                janela.update()
+
+#
+# 
+#-------------------------------------------------------------------------------#
+             
+#
+# Layout do sistema
+#-------------------------------------------------------------------------------#
+
+def verificaAP():
+    ip = iphost(et.get())
+    verifAparelho(ip,161,165,80,86)
+
+def verificaRD():
+    ip = iphost(et.get())
+    verifAparelho(ip,166,175,87,89)
+
+def verificaTM():
+    ip = iphost(et.get())
+    verifAparelho(ip,176,189,234,246)
+
+def verificaTD():
+    ip = iphost(et.get())
+    verificaAP()
+    verificaTM()
+    verificaRD()
+
+# configuração da janela
+janela = Tk()
+janela.geometry("485x400+400+200")
+janela["bg"] = "#2196f3"
+janela.title("Scan de Rede - Nimal Tecnologia")
+
+# config botões
+btn1 = Button(janela, text="Access point", bg="white", command=verificaAP)
+btn2 = Button(janela, text="Rádio Frequencia", bg="white", command=verificaRD)
+btn3 = Button(janela, text="Terminal de consulta", bg="white", command=verificaTM)
+btn4 = Button(janela, text="Todos", bg="white", command=verificaTD)
+
+btn1.place(x=10, y=40)
+btn2.place(x=90, y=40)
+btn3.place(x=195, y=40)
+btn4.place(x=320, y=40)
+
+# label
+lb = Label(janela, text="Número da loja:")
+lb.place(x=10, y=10)
+
+visor = Text(janela, width="58", height=20)
+visor.place(x=10, y=70)
+
+
+# entry
+et = Entry(janela, width=5)
+et.place(x=110,y=10)
+
+janela.mainloop()
+#------------------------------------------------------------------------------#
